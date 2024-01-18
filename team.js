8752bf3bea137ca2a618727e751fca640894273c
miguel201293 <miguel.abato.93@gmail.com>
2024-01-18 15:49:10 +0100

    Se aplican cambios

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-let students = []
+let students = [{
+    name:"Miguel",
+    surname:"Abato",
+    age:30,
+    city:"Sevilla",
+    hobby:"Gimnasio",
+    favoriteFood:"Pasta",
+    favoriteVideoGame:"FIFA",
+    favoriteFilm:"Señor de los anillos",
+    favoriteBook:"Imperio final",
+    petName: "Lupo"
+}]
