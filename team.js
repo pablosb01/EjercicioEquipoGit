b26862321016d48b19717df0e4b37a0b65b06af6
pablosb01 <pablosb01@icloud.com>
2024-01-19 11:41:10 +0100

    datos pablo

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-let students = []
+let students = [{
+    name: "Pablo",
+    surname: "Diaz",
+    age: "22",
+    city: "Villamayor",
+    hobby: "Ski",
+    favoriteFood: "SmashBurger",
+    favoriteVideogame: "Escape From Tarkov",
+    favoriteFilm: "Eyes Wide Shut",
+    favoriteBook: "El Visitante",
+    petName: "Tango",
+}]
